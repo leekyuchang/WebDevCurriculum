ca6ae0b98aa70e3235bc9cf8a65732068bf71bfd
leekyuchang <kyuchang90@naver.com>
2016-01-25 15:05:23 +0900

    skeleton

---
+++ Part 2. JavaScript/Quest 04. Hello, JavaScript/skeleton/skeleton.js
@@ -0,0 +1,27 @@
+var number = prompt("Please input number.");
+
+var star = "";
+
+var plus = function() {
+
+    if (isNaN(number)) {
+        console.log("Please input the correct number");
+    }
+    else if (number > 0){
+        console.log("number: " + number);
+
+        for (var i = 0; i < number; i += 1){
+            star +="*";
+            console.log(star);
+        }
+
+    }else {
+        console.log("number: " + number);
+        return 0;
+    }
+
+}
+
+plus();
+
+
