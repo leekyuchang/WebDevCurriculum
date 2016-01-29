19334ef1ea6c28de6d981a5ca268484e4c81c20b
leekyuchang <kyuchang90@naver.com>
2016-01-29 18:01:13 +0900

    skeleton

--- Part 2. JavaScript/Quest 06. JavaScript OOP - Design/skeleton/desktop.js
+++ Part 2. JavaScript/Quest 06. JavaScript OOP - Design/skeleton/desktop.js
@@ -4,12 +4,17 @@ var Desktop = function() {
 
 var Icon = function() {
 	/* TODO: Icon 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+
 };
 
 var Folder = function() {
 	/* TODO: Folder 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+	//open함수
 };
 
 var Window = function() {
 	/* TODO: Window 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+
 };
+
+//drag함수를 Icon,, Folder, Window 클래스에서 사용가능하게
