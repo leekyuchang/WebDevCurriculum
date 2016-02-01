19a333a4041831d7bef48aac2c8c76c73a79d2ce
leekyuchang <kyuchang90@naver.com>
2016-02-01 12:58:05 +0900

    skeleton

--- Part 2. JavaScript/Quest 06. JavaScript OOP - Design/skeleton/desktop.js
+++ Part 2. JavaScript/Quest 06. JavaScript OOP - Design/skeleton/desktop.js
@@ -2,7 +2,7 @@ var Desktop = function(icon, folder) {
 	/* TODO: Desktop 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
 	this.icon = function(){};
 	this.folder = function(){};
-	//Icon, Folder를 매게변수로 넘겨서 생성
+	//Icon, Folder를 매게변수로 넘겨서 갯수 만큼 생성
 };
 
 var Icon = function(name) {
@@ -13,22 +13,26 @@ var Icon = function(name) {
 
 var Folder = function(name) {
 	/* TODO: Folder 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
-	Icon.call(this, name)
+	Icon.call(this, name);
+	//drag함수 상속
 	this.prototype.clickopen = function(){}
 };
 
 
-var Window = function(name) {
+var Window = function() {
 	/* TODO: Window 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
-	Folder.call(this, name)
+	//window의 name과 Folder의 name이 같게 표현
+	//drag함수 상속
+	Folder.call(this);
 };
 
-var Icon1 = new Icon("icon1");
-var Folder1 = new Folder("folder1");
-var Folder1 = new Folder("folder2");
+delete Window.prototype.clickopen(); //window의 clickopen함수 제거
+var icon1 = new Icon();
+var folder1 = new Folder();
+var folder2 = new Folder();
 
-delete Window.prototype.clickopen();
 
-//변수name(개별name 상속X)
+
+//변수name(개별name)
 //함수drag(Icon -> Folder -> Window 상속관계)
 //함수clickOpen(Folder만)
