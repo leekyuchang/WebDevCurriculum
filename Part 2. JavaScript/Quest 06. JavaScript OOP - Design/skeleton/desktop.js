4b63b65aa1aaf0d59c339f1d464be78950346af0
leekyuchang <kyuchang90@naver.com>
2016-02-02 16:17:17 +0900

    system design

--- Part 2. JavaScript/Quest 06. JavaScript OOP - Design/skeleton/desktop.js
+++ Part 2. JavaScript/Quest 06. JavaScript OOP - Design/skeleton/desktop.js
@@ -15,35 +15,37 @@ var Desktop = function(iconname, foldername) {
 	for(var j = 0; j < iconname.length; j++){
 		icon[j] = new Icon(iconname[j]);
 	}
-
 };
+
 
 /*  Icon 생성자  */
 var Icon = function(name) {
 	/* TODO: Icon 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
 	//멤버변수: name
-	//멤버함수: prototype.drag, createclass
+	//멤버함수: prototype.drag, prototype.createclass
 	this.name = name;
 };
 Icon.prototype.drag = function(){alert("drag");};
 Icon.prototype.createclass = function(){};  // 인스턴스들의 CSS class 생성 후 추가
 
+
 /*  Folder 생성자  */
 var Folder = function(name) {
 	/* TODO: Folder 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
 	//멤버변수: name
-	//멤버함수: clickopen
+	//멤버함수: drag(상속), clickopen, createclass(상속)
 	this.name = name;
 	this.clickopen = function(){alert("open");};
 	//click하면 window인스턴스 생성, Folder만 쓰는 clickopen함수
 };
 Folder.prototype = Icon.prototype; //drag, createclass함수 상속
 
+
 /*  Window 생성자  */
 var Window = function(name) {
 	/* TODO: Window 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
-	//멤버변수:
-	//멤버함수: prototype.drag
-	// Folder의 인스턴스와 같은 name이어야 한다.
+	//멤버변수: name
+	//멤버함수: drag(상속), createclass(상속)
+	this.name = name;  // Folder의 인스턴스와 같은 name이어야 한다.
 };
 Window.prototype = Icon.prototype;  //drag, createclass함수 상속
