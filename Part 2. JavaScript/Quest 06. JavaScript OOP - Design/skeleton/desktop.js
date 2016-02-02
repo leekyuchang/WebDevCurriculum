5503cab58f17ed36e01eee6ed97be9ae2a07bc67
leekyuchang <kyuchang90@naver.com>
2016-02-02 15:57:07 +0900

    system design

--- Part 2. JavaScript/Quest 06. JavaScript OOP - Design/skeleton/desktop.js
+++ Part 2. JavaScript/Quest 06. JavaScript OOP - Design/skeleton/desktop.js
@@ -1,43 +1,47 @@
-var Desktop = function(foldername) {
+var Desktop = function(iconname, foldername) {
 	/* TODO: Desktop 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+
+	//Desktop에서 인스턴스를 생성하면 icon, folder의 인스턴스들이 생성되게 구현
+	//멤버변수: Icon, Folder의 인스턴스를 생성할 변수들,
+	//멤버함수:
+
 	folder = new Array();
 	icon = new Array();
-	var folderclass = document.querySelector('.desktop');
-	var strHTML = "";
+
 	for(var i = 0; i < foldername.length; i++){
 		folder[i] = new Folder(foldername[i]);
-		strHTML += '<div class="folder' + i + '"></div>';
 	}
-	folderclass.innerHTML = strHTML;
+
+	for(var j = 0; j < iconname.length; j++){
+		icon[j] = new Icon(iconname[j]);
+	}
 
 };
 
-
-
-
-////////////////////////////////////////////////////////////////////////////////
-
-
+/*  Icon 생성자  */
 var Icon = function(name) {
 	/* TODO: Icon 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+	//멤버변수: name
+	//멤버함수: prototype.drag, 
 	this.name = name;
 };
-//drag 함수 Icon prototype에 생성
+Icon.prototype.drag = function(){alert("drag");};
 
+/*  Folder 생성자  */
 var Folder = function(name) {
 	/* TODO: Folder 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
-	this.name = name
+	//멤버변수: name
+	//멤버함수: clickopen
+	this.name = name;
+	this.clickopen = function(){alert("open");};  //Folder만 쓰는 clickopen함수 생성
 };
+Folder.prototype = Icon.prototype; //drag함수 상속
 
-var Window = function() {
+/*  Window 생성자  */
+var Window = function(name) {
 	/* TODO: Window 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
-	//window의 name과 Folder의 name이 같게 표현 - ?
+	//멤버변수:
+	//멤버함수: prototype.drag
+	// Folder의 인스턴스와 같은 name이어야 한다.
 };
-
-
-// delete Window.prototype.clickopen(); //window의 clickopen함수 제거
-// var folder1 = new Folder('1folder');
-// var window1 = new Folder(folder1.name); //Folder's instance name == windos's instance name
-//변수name(개별name)
-//함수drag(Icon -> Folder -> Window 상속관계)
-//함수clickOpen(Folder만)
+Window.prototype = Icon.prototype;  //drag함수 상속
--- Part 2. JavaScript/Quest 06. JavaScript OOP - Design/skeleton/test.js
+++ Part 2. JavaScript/Quest 06. JavaScript OOP - Design/skeleton/test.js
@@ -62,3 +62,96 @@ Icon.prototype.allt = function(){ alert(this.name);}
 
 var desk1 = new Desktop(['lee'], ['kyu', 'chang']);
 /////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+/////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+/////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+
+var Desktop = function(iconname, foldername) {
+	/* TODO: Desktop 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+	folder = new Array();
+	icon = new Array();
+
+	var folderclass = document.querySelector('.desktop');
+	var strHTML = "";
+
+	for(var i = 0; i < foldername.length; i++){
+		folder[i] = new Folder(foldername[i]);
+		strHTML += '<div class="' + foldername[i] + '"></div>';
+	}
+	folderclass.innerHTML = strHTML;
+
+};
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+var Icon = function(name) {
+	/* TODO: Icon 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+	this.name = name;
+};
+
+
+var Folder = function(name) {
+	/* TODO: Folder 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+	this.name = name
+};
+
+// Folder.prototype.changecolor = function(){
+// 	document.querySelector(".folder[1]").innerHTML = "folder";
+// 	// for(var i = 0; i < folder.length; i++){
+// 	// 	document.querySelector(".folder[i]").innerHTML = "folder";
+// 	// }
+// }
+
+var Window = function(name) {
+	/* TODO: Window 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+	//window의 name과 Folder의 name이 같게 표현 - ?
+
+};
+
+
+// delete Window.prototype.clickopen(); //window의 clickopen함수 제거
+// var folder1 = new Folder('1folder');
+// var window1 = new Folder(folder1.name); //Folder's instance name == windos's instance name
+//함수drag(Icon -> Folder -> Window 상속관계)
+//함수clickOpen(Folder만)
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+/////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+
+
+
+var Icon = function(name) {
+	this.name = name;
+};
+Icon.prototype.drag = function(){alert("drag");};
+Icon.prototype.getName = function(){alert(this.name);};
+
+
+var Folder = function(name) {
+	this.name = name;
+	this.clickopen = function(){alert("open");};
+};
+
+Window.prototype = new Icon();
+
+var Window = function(name) {
+	// Folder의 인스턴스와 같은 name
+	//clickopen은 없어야됨
+};
+Window.prototype = Icon.prototype;
+
+var icon1 = new Icon("1icon");
+var folder1 = new Folder("1folder");
+var folder2 = new Folder("2folder");
+var window1 = new Window("1window");
