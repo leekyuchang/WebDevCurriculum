e875f8f3d8b9476821bcdbd905c9c48296958f06
kyu <kyu@kyu-2.local>
2016-03-07 13:47:23 +0900

    test

--- Part 3. node.js/Quest 12. AJAX/skeleton/client/notepad.js
+++ Part 3. node.js/Quest 12. AJAX/skeleton/client/notepad.js
@@ -1,3 +1,16 @@
-var Notepad = function() {
+var Notepad = function(dom) {
 	/* TODO: 그 외에 또 어떤 클래스와 메소드가 정의되어야 할까요? */
+	this._initialize();
+	this.dom = dom;
+};
+
+Notepad.prototype._initialize = function(){
+	this._setDom();
+};
+
+
+Notepad.prototype._setDom = function(){
+	// this.dom = document.querySelector('.templates .toolboxTemplate').cloneNode(true);
+	this.dom = dom.cloneNode(true);
+	this.dom.style.display = 'block';
 };
