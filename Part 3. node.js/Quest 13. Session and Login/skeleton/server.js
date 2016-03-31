25dac5860ebc171cc086c775f4ddd52384935f82
leekyuchang <kyuchang90@gmail.com>
2016-03-31 10:49:18 +0900

    apply to notepad

--- Part 3. node.js/Quest 13. Session and Login/skeleton/server.js
+++ Part 3. node.js/Quest 13. Session and Login/skeleton/server.js
@@ -1,14 +1,51 @@
 var express = require('express'),
 	path = require('path'),
+	fs = require('fs'),
+	bodyParser = require('body-parser');
 	app = express();
 
-app.use(express.static('client'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/static', express.static('static'));
 
 app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, 'index.html'));
+	res.sendFile(path.join(__dirname, 'static/index.html'));
 });
 
-/* TODO: 여기에 처리해야 할 요청의 주소별로 동작을 채워넣어 보세요..! */
+app.get('/load', function(req, res) {
+	var dir = path.join(__dirname, 'notes'),
+		files = fs.readdirSync(dir);
+
+	var matched = files.filter(function(f) {
+			return (
+				JSON.parse(
+					fs.readFileSync(
+						path.join(dir, f),
+						'utf-8'
+					)
+				).name === req.query.name
+			);
+		})[0];
+
+	if(matched) {
+		var obj = JSON.parse(fs.readFileSync(path.join(dir, matched), 'utf-8'));
+		obj.id = Number(matched.replace('.json', ''));
+		obj.success = true;
+		res.json(obj);
+	} else {
+		var obj = { success: false };
+		res.json(obj);
+	}
+});
+
+app.post('/save', function(req, res) {
+	var dir = path.join(__dirname, 'notes'),
+		data = JSON.parse(JSON.stringify(req.body));
+
+	data.id = Number(data.id);
+
+	fs.writeFileSync(path.join(dir, data.id + '.json'), JSON.stringify(data, null, 4), 'utf-8');
+	res.send('success');
+});
 
 var server = app.listen(8080, function () {
 	console.log('Server started!');
---
+++ Part 3. node.js/Quest 13. Session and Login/skeleton/static/notepad.js
@@ -0,0 +1,266 @@
+var Notepad = function(dom) {
+		this.dom = dom;
+
+		this._initialize();
+	};
+
+var _ = Notepad.prototype;
+
+_._initialize = function() {
+	this._setSubObjects();
+	this._setDom();
+	this._bindEvents();
+};
+
+_._setSubObjects = function() {
+	this.menu = new Menu();
+	this.tabs = new Tabs();
+	this.memoForm = new MemoForm();
+};
+
+_._setDom = function() {
+	var tmpl = document.querySelector('.templates .notepad-inst');
+
+	this.dom.innerHTML = '';
+	this.dom.appendChild(tmpl.cloneNode(true));
+
+	this.dom.querySelector('.menu').appendChild(this.menu.dom);
+	this.tabs.dom = this.dom.querySelector('.tabs');
+	this.dom.querySelector('.memo-form').appendChild(this.memoForm.dom);
+};
+
+_._bindEvents = function() {
+	var that = this;
+
+	this.menu.dom.addEventListener('newTab', function() {
+		that.tabs.newTab();
+	});
+
+	this.menu.dom.addEventListener('loadTab', function() {
+		var name = prompt('Input tab name:');
+		that.tabs.loadTab(name);
+	});
+
+	this.tabs.dom.addEventListener('selectTab', function(e) {
+		that.tabs.selectedTab = e.tab;
+		that.memoForm.render(e.data);
+	});
+
+	this.memoForm.dom.addEventListener('saveTab', function(e) {
+		e.data.id = that.tabs.selectedTab.data.id;
+		// server save
+		var req = new XMLHttpRequest();
+		req.open('POST', '/save');
+		req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+		req.body = '';
+		req.body += 'id=' + e.data.id + '&';
+		req.body += 'name=' + e.data.name + '&';
+		req.body += 'content=' + e.data.content;
+		req.onreadystatechange = function (aEvt) {
+			if (req.readyState == 4) {
+				if(req.status == 200) {
+					that.tabs.selectedTab.saveData(e.data);
+				} else {
+					console.log('error');
+				}
+			}
+		};
+		req.send(req.body);
+	});
+};
+
+
+
+
+
+
+var Menu = function() {
+		this._initialize();
+	};
+
+_ = Menu.prototype;
+
+_._initialize = function() {
+	this._setDom();
+	this._bindEvents();
+};
+
+_._setDom = function() {
+	var tmpl = document.querySelector('.templates .menu-inst');
+
+	this.dom = tmpl.cloneNode(true);
+};
+
+_._bindEvents = function() {
+	var that = this;
+
+	this.dom.querySelector('.new').addEventListener('click', function() {
+		that.dom.dispatchEvent(new Event('newTab'));
+	});
+
+	this.dom.querySelector('.load').addEventListener('click', function() {
+		that.dom.dispatchEvent(new Event('loadTab'));
+	});
+};
+
+
+
+
+
+
+
+var Tabs = function() {
+		this.dom;
+		this.selectedTab;
+		this.tabs = [];
+	};
+
+_ = Tabs.prototype;
+
+_.newTab = function() {
+	var tab = new Tab();
+	this._addTab(tab);
+};
+
+_.loadTab = function(name) {
+	var that = this;
+
+	var req = new XMLHttpRequest();
+	req.open('GET', '/load?name=' + name);
+	req.onreadystatechange = function (aEvt) {
+		if (req.readyState == 4) {
+			if(req.status == 200) {
+				var data = JSON.parse(req.responseText);
+
+				if(data.success) {
+					var tab = new Tab(data);
+					that._addTab(tab);
+				} else {
+					console.log('error: no memo with that name');
+				}
+			} else {
+				console.log('error');
+			}
+		}
+	};
+	req.send(null);
+};
+
+_._addTab = function(tab) {
+	var that = this;
+
+	this.tabs.push(tab);
+	this.dom.appendChild(tab.dom);
+	tab.dom.dispatchEvent(new Event('click'));
+	tab.dom.addEventListener('closeTab', function() {
+		var targetIdx = null;
+
+		that.tabs.forEach(function(t, idx) {
+			if(t.data.id === tab.data.id) {
+				targetIdx = idx;
+			}
+		});
+
+		if(targetIdx) {
+			that.tabs[targetIdx].kill();
+			that.tabs.splice(targetIdx, 1);
+		}
+	});
+	this.selectedTab = tab;
+};
+
+
+
+
+
+var Tab = function(data) {
+		if(data === undefined) {
+			data = {
+				id: Math.floor(Math.random() * 1000000),
+				name: 'New tab',
+				content: ''
+			};
+		}
+		this.data = data;
+		this._initialize();
+	};
+
+_ = Tab.prototype;
+
+_._initialize = function() {
+	this._setDom();
+	this._bindEvents();
+};
+
+_._setDom = function() {
+	var tmpl = document.querySelector('.templates .tab-inst');
+
+	this.dom = tmpl.cloneNode(true);
+	this.dom.querySelector('.name').innerHTML = this.data.name;
+};
+
+_._bindEvents = function() {
+	var that = this;
+
+	this.dom.addEventListener('click', function() {
+		var ev = new Event('selectTab', { "bubbles": true });
+		ev.data = that.data;
+		ev.tab = that;
+		that.dom.dispatchEvent(ev);
+	});
+
+	this.dom.querySelector('.close').addEventListener('click', function(e) {
+		e.stopPropagation();
+		var ev = new Event('closeTab');
+		that.dom.dispatchEvent(ev);
+	});
+};
+
+_.kill = function() {
+	this.dom.remove();
+};
+
+_.saveData = function(data) {
+	this.data = data;
+	this.dom.querySelector('.name').innerHTML = data.name;
+}
+
+
+
+
+var MemoForm = function() {
+		this._initialize();
+	};
+
+_ = MemoForm.prototype;
+
+_._initialize = function() {
+	this._setDom();
+	this._bindEvents();
+};
+
+_._setDom = function() {
+	var tmpl = document.querySelector('.templates .memo-form-inst');
+
+	this.dom = tmpl.cloneNode(true);
+};
+
+_._bindEvents = function() {
+	var that = this;
+
+	this.dom.querySelector('.save').addEventListener('click', function() {
+		var ev = new Event('saveTab');
+		ev.data = {
+			name: that.dom.querySelector('.name').value,
+			content: that.dom.querySelector('.content').value
+		};
+		that.dom.dispatchEvent(ev);
+	});
+};
+
+_.render = function(data) {
+	this.dom.style.display = 'block';
+
+	this.dom.querySelector('.name').value = data.name;
+	this.dom.querySelector('.content').value = data.content;
+};
