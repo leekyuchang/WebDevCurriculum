69a48978ba586807d34a95eb5b0b70ae9f07fb86
leekyuchang <kyuchang90@gmail.com>
2016-03-31 11:40:33 +0900

    login, but session data store not yet

--- Part 3. node.js/Quest 13. Session and Login/skeleton/server.js
+++ Part 3. node.js/Quest 13. Session and Login/skeleton/server.js
@@ -1,15 +1,30 @@
 var express = require('express'),
 	path = require('path'),
 	fs = require('fs'),
+	session = require('express-session'),
 	bodyParser = require('body-parser');
 	app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+	secret : '!@#asdf!@#gre',
+	resave : false,
+	saveUninitialized : true
+}));
+
 app.use('/static', express.static('static'));
 
+
 app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, 'static/index.html'));
+	if(req.session.username) {
+        console.log('logined');
+		res.sendFile(path.join(__dirname, 'static/index.html'));
+        // res.send('<a href="/logout">logout</a>');
+    } else {
+        res.send('You should login. ' + '<a href="/login">login</a>');
+    }
 });
+
 
 app.get('/load', function(req, res) {
 	var dir = path.join(__dirname, 'notes'),
@@ -37,6 +52,7 @@ app.get('/load', function(req, res) {
 	}
 });
 
+
 app.post('/save', function(req, res) {
 	var dir = path.join(__dirname, 'notes'),
 		data = JSON.parse(JSON.stringify(req.body));
@@ -45,6 +61,58 @@ app.post('/save', function(req, res) {
 
 	fs.writeFileSync(path.join(dir, data.id + '.json'), JSON.stringify(data, null, 4), 'utf-8');
 	res.send('success');
+});
+
+
+app.get('/login', function(req, res) {
+    res.sendFile(path.join(__dirname + '/static', 'login.html'));
+});
+
+
+app.post('/login', function(req, res) {
+	var a = 0;
+	var i;
+	var uname = req.body.username;
+	var pwd = req.body.password;
+	var user = [
+		{
+			username : 'test1',
+			password : 'test1'
+		},
+		{
+			username : 'test2',
+			password : 'test2'
+		},
+		{
+			username : 'test3',
+			password : 'test3'
+		}
+	];
+
+	function checkExistName(array, name, password) {
+        for(i = 0; i < array.length; i += 1) {
+            if(array[i].username === name && array[i].password === password) {
+                a = 1;
+				// return i;
+            }
+        }
+    }
+
+    checkExistName(user, uname, pwd);
+
+    if(a === 1) {
+        // req.session.logined = true;
+		req.session.username = uname;
+        res.redirect('/');
+    } else {
+        res.send('you"re not member' + '<a href="/login">back</a>');
+    }
+});
+
+
+app.get('/logout', function(req, res) {
+    req.session.destroy();
+    res.redirect('/login');
 });
 
 var server = app.listen(8080, function () {
