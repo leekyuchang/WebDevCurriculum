a1f85134fd5d10d7de7c9d5e44f30ae9ca036ef1
leekyuchang <kyuchang90@gmail.com>
2016-04-08 11:50:40 +0900

    remove

--- Part 4. Database/Quest 15. ORM/skeleton/client/notepad.js
+++ Part 4. Database/Quest 15. ORM/skeleton/client/notepad.js
@@ -1,3 +1,357 @@
-var Notepad = function() {
-	/* TODO: 그 외에 또 어떤 클래스와 메소드가 정의되어야 할까요? */
+var Notepad = function(dom) {
+		this.dom = dom;
+		this._initialize();
+	};
+
+var _ = Notepad.prototype;
+
+_._initialize = function() {
+	this._setSubObjects();
+	this._setDom();
+	this._bindEvents();
+};
+
+_._setSubObjects = function() {
+
+	this.logins = new Login();
+	this.menu = new Menu();
+	this.tabs = new Tabs();
+	this.memoForm = new MemoForm();
+};
+
+_._setDom = function() {
+	var that = this;
+	var tmpl = document.querySelector('.templates .notepad-inst');
+
+	this.dom.innerHTML = '';
+	this.dom.appendChild(tmpl.cloneNode(true));
+
+	this.dom.querySelector('.login').appendChild(this.logins.dom);
+	this.dom.querySelector('.menu').appendChild(this.menu.dom);
+
+	this.tabs.dom = this.dom.querySelector('.tabs');
+	this.dom.querySelector('.memo-form').appendChild(this.memoForm.dom);
+};
+
+_._bindEvents = function() {
+	var that = this;
+
+	this.menu.dom.addEventListener('newTab', function() {
+		that.tabs.newTab();
+	});
+
+	this.menu.dom.addEventListener('loadTab', function() {
+		var name = prompt('Input tab name:');
+		that.tabs.loadTab(name);
+	});
+
+	this.tabs.dom.addEventListener('selectTab', function(e) {
+		that.tabs.selectedTab = e.tab;
+		that.memoForm.render(e.data);
+	});
+
+	this.memoForm.dom.addEventListener('saveTab', function(e) {
+		e.data.id = that.tabs.selectedTab.data.id;
+		var req = new XMLHttpRequest();
+		req.open('POST', '/save');
+		req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+		req.body = '';
+		req.body += 'id=' + e.data.id + '&';
+		req.body += 'name=' + e.data.name + '&';
+		req.body += 'content=' + e.data.content;
+		req.onreadystatechange = function (aEvt) {
+			if (req.readyState == 4) {
+				if(req.status == 200) {
+					that.tabs.selectedTab.saveData(e.data);
+				} else {
+					console.log('error');
+				}
+			}
+		};
+		req.send(req.body);
+	});
+
+	this.logins.dom.addEventListener('loginCheck', function(e) {
+
+		if(e.trueOrfalse !== 'false') {
+			that.logins.logouttmpl.style.display = 'block';
+			var data = JSON.parse(e.trueOrfalse);
+			var tablength = data.tabnumbers;
+			if (tablength == 1) {
+				if (data.tabname === "New tab") {
+					that.tabs.newTab();
+				} else {
+					that.tabs.loadTab(data.tabname);
+				}
+			} else if (tablength > 1) {
+				for(var i = 0; i < tablength; i++) {
+					if(data.tabname[i] === "New tab") {
+						that.tabs.newTab();
+					} else {
+						that.tabs.loadTab(data.tabname[i]);
+					}
+				}
+			}
+		} else if (e.trueOrfalse === 'false') {
+			that.logins.logintmpl.style.display = 'block';
+		}
+	});
+};
+
+
+
+
+
+
+
+var Menu = function() {
+		this._initialize();
+	};
+
+_ = Menu.prototype;
+
+_._initialize = function() {
+	this._setDom();
+	this._bindEvents();
+};
+
+_._setDom = function() {
+	var tmpl = document.querySelector('.templates .menu-inst');
+	this.dom = tmpl.cloneNode(true);
+};
+
+_._bindEvents = function() {
+	var that = this;
+
+	this.dom.querySelector('.new').addEventListener('click', function() {
+		that.dom.dispatchEvent(new Event('newTab'));
+	});
+
+	this.dom.querySelector('.load').addEventListener('click', function() {
+		that.dom.dispatchEvent(new Event('loadTab'));
+	});
+};
+
+
+
+
+
+
+var Tabs = function() {
+		this.dom;
+		this.selectedTab;
+		this.tabs = [];
+	};
+
+_ = Tabs.prototype;
+
+_.newTab = function() {
+	var tab = new Tab();
+	this._addTab(tab);
+};
+
+_.loadTab = function(name) {
+	var that = this;
+
+	var req = new XMLHttpRequest();
+	req.open('GET', '/load?name=' + name);
+	req.onreadystatechange = function (aEvt) {
+		if (req.readyState == 4) {
+			if(req.status == 200) {
+				var data = JSON.parse(req.responseText);
+				if(data.success) {
+					var tab = new Tab(data);
+					that._addTab(tab);
+				} else {
+					console.log('error: no memo with that name');
+				}
+			} else {
+				console.log('error');
+			}
+		}
+	};
+	req.send(null);
+
+};
+
+_._addTab = function(tab) {
+	var that = this;
+
+	// addtab
+	this.tabs.push(tab);
+	this.dom.appendChild(tab.dom);
+
+	function checkAndSend() {
+		var req = new XMLHttpRequest();
+		req.open('POST', '/tabsave');
+		req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+		req.body = '';
+		for(var i=0; i < that.tabs.length; i++) {
+			var currentTab = that.tabs[i].dom.firstElementChild.innerText;
+			req.body += 'tabname=' + currentTab + '&';
+		}
+		 req.body += 'tabnumbers=' + that.tabs.length;
+
+		req.onreadystatechange = function () {
+			if (req.readyState == 4) {
+				if(req.status == 200) {
+					console.log('good');
+				} else {
+					console.log('error');
+				}
+			}
+		};
+		req.send(req.body);
+	}
+	checkAndSend();
+
+	tab.dom.addEventListener('closeTab', function() {
+		var targetIdx = null;
+		that.tabs.forEach(function(t, idx) {
+			if(t.data.id === tab.data.id) {
+				targetIdx = idx;
+			}
+		});
+		if(targetIdx + 1) {
+			that.tabs[targetIdx].kill();
+			that.tabs.splice(targetIdx, 1);
+		}
+		checkAndSend();
+	});
+	this.selectedTab = tab;
+};
+
+
+
+
+var Tab = function(data) {
+		if(data === undefined) {
+			data = {
+				id: Math.floor(Math.random() * 1000000),
+				name: 'New tab',
+				content: ''
+			};
+		}
+		this.data = data;
+		this._initialize();
+	};
+
+_ = Tab.prototype;
+
+_._initialize = function() {
+	this._setDom();
+	this._bindEvents();
+};
+
+_._setDom = function() {
+	var tmpl = document.querySelector('.templates .tab-inst');
+
+	this.dom = tmpl.cloneNode(true);
+	this.dom.querySelector('.name').innerHTML = this.data.name;
+};
+
+_._bindEvents = function() {
+	var that = this;
+
+	this.dom.addEventListener('click', function() {
+		var ev = new Event('selectTab', { "bubbles": true });
+		ev.data = that.data;
+		ev.tab = that;
+		that.dom.dispatchEvent(ev);
+	});
+
+	this.dom.querySelector('.close').addEventListener('click', function(e) {
+		e.stopPropagation();
+		var ev = new Event('closeTab');
+		that.dom.dispatchEvent(ev);
+	});
+};
+
+_.kill = function() {
+	this.dom.remove();
+};
+
+_.saveData = function(data) {
+	this.data = data;
+	this.dom.querySelector('.name').innerHTML = data.name;
+}
+
+
+
+
+
+
+var MemoForm = function() {
+		this._initialize();
+	};
+
+_ = MemoForm.prototype;
+
+_._initialize = function() {
+	this._setDom();
+	this._bindEvents();
+};
+
+_._setDom = function() {
+	var tmpl = document.querySelector('.templates .memo-form-inst');
+	this.dom = tmpl.cloneNode(true);
+};
+
+_._bindEvents = function() {
+	var that = this;
+
+	this.dom.querySelector('.save').addEventListener('click', function() {
+		var ev = new Event('saveTab');
+		ev.data = {
+			name: that.dom.querySelector('.name').value,
+			content: that.dom.querySelector('.content').value
+		};
+		that.dom.dispatchEvent(ev);
+	});
+};
+
+_.render = function(data) {
+	this.dom.style.display = 'block';
+	this.dom.querySelector('.name').value = data.name;
+	this.dom.querySelector('.content').value = data.content;
+};
+
+
+
+var Login = function() {
+	this._initialize();
+};
+
+_ = Login.prototype;
+
+_._initialize = function() {
+	this._setDom();
+	this._bindEvents();
+};
+
+_._setDom = function() {
+
+	var tmpl = document.querySelector('.templates .login-inst');
+	this.dom = tmpl.cloneNode(true);
+	this.logintmpl = this.dom.querySelector('.login-form');
+	this.logouttmpl = this.dom.querySelector('.logout-form');
+};
+
+_._bindEvents = function() {
+	var that = this;
+
+	// window.addEventListener('load', function() {
+	var ev = new Event('loginCheck');
+	ev.req = new XMLHttpRequest();
+	ev.req.open('GET', '/logined');
+	ev.req.onreadystatechange = function() {
+		if (ev.req.readyState == 4) {
+			if (ev.req.status == 200) {
+				ev.trueOrfalse = ev.req.responseText;
+				that.dom.dispatchEvent(ev);
+			}
+		}
+	};
+	ev.req.send(null);
 };
--- Part 4. Database/Quest 15. ORM/skeleton/server.js
+++ Part 4. Database/Quest 15. ORM/skeleton/server.js
@@ -1,15 +1,117 @@
 var express = require('express'),
-	Sequelize = require('sequelize'),
 	path = require('path'),
+	fs = require('fs'),
+	session = require('express-session'),
+	bodyParser = require('body-parser');
 	app = express();
 
-app.use(express.static('client'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+	secret : '!@#asdf!@#gre',
+	resave : true,
+	saveUninitialized : false
+}));
+
+app.use('/static', express.static('static'));
+
+var user = [
+	{	uid : 'test1',
+		username : 'test1',
+		password : 'test1'},
+	{	uid : 'test2',
+		username : 'test2',
+		password : 'test2'},
+	{	uid : 'test3',
+		username : 'test3',
+		password : 'test3'}
+];
 
 app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, 'index.html'));
+	res.sendFile(path.join(__dirname, 'static/index.html'));
 });
 
-/* TODO: 여기에 처리해야 할 요청의 주소별로 동작을 채워넣어 보세요..! */
+app.get('/load', function(req, res) {
+	var dir = path.join(__dirname, 'notes', req.session.username),
+		files = fs.readdirSync(dir);
+
+	var matched = files.filter(function(f) {
+			return (
+				JSON.parse(
+					fs.readFileSync(
+						path.join(dir, f),
+						'utf-8'
+					)
+				).name === req.query.name
+			);
+		})[0];
+
+	if(matched) {
+		var obj = JSON.parse(fs.readFileSync(path.join(dir, matched), 'utf-8'));
+		obj.id = Number(matched.replace('.json', ''));
+		obj.success = true;
+		res.json(obj);
+	} else {
+		var obj = { success: false };
+		res.json(obj);
+	}
+});
+
+
+app.post('/save', function(req, res) {
+	var dir = path.join(__dirname, 'notes', req.session.username),
+		data = JSON.parse(JSON.stringify(req.body));
+	data.id = Number(data.id);
+	// req.session.data = data;
+	fs.writeFileSync(path.join(dir, data.id + '.json'), JSON.stringify(data, null, 4), 'utf-8');
+	res.send('success');
+});
+
+app.post('/tabsave', function(req, res) {
+	if (req.session.username !== undefined) {
+		var dir = path.join(__dirname, 'notes'),
+		data = JSON.parse(JSON.stringify(req.body));
+		console.log(data);
+		fs.writeFileSync(path.join(dir, req.session.username + '.json'), JSON.stringify(data, null, 4), 'utf-8');
+		res.send('good');
+	}
+});
+
+
+app.get('/logined', function(req, res) {
+	if (req.session.username) {
+		var tabDataDir = path.join(__dirname, 'notes', req.session.username + '.json'),
+		tabData = fs.readFileSync(tabDataDir, 'utf-8');
+		res.send(tabData);
+
+	} else {
+		res.send('false');
+	}
+});
+
+app.post('/login', function(req, res) {
+	var data = JSON.parse(JSON.stringify(req.body));
+	var uname = data.username;
+	var pwd = data.password;
+
+	for(var j=0; j<user.length; j++) {
+		var users = user[j];
+		if(uname === users.username && pwd === users.password) {
+			req.session.username = uname;
+			req.session.save(function() {
+				res.redirect('/');
+			});
+		} else if (uname === users.username && pwd !== users.password) {
+			res.send('false');
+		}
+	}
+});
+
+app.post('/logout', function(req, res) {
+	req.session.destroy(function() {
+		res.redirect('/');
+	});
+
+});
 
 var server = app.listen(8080, function () {
 	console.log('Server started!');
